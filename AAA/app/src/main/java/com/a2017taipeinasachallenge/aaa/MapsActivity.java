21a30e4fa9f177255929a1ca75d9894e11e238e9
王士哲 <S352431@yo-woo.com>
2017-04-30 08:25:43 +0800

    upload & download data

--- AAA/app/src/main/java/com/a2017taipeinasachallenge/aaa/MapsActivity.java
+++ AAA/app/src/main/java/com/a2017taipeinasachallenge/aaa/MapsActivity.java
@@ -9,14 +9,17 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,28 +33,41 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Random;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+    private final String SEND_POINTS_URL = "http://59.124.23.150:5566/FightFires/map/postAreaData";
+    private final String GET_POINTS_URL = "http://59.124.23.150:5566/FightFires/map/getAreaData";
     final int MY_PERMISSION_ACCESS_COURSE_LOCATION = 0x1;
     private GoogleMap mMap;
     private Double longitude = 0.0;
     private Double latitude = 0.0;
     private Sensor mSensor;
+    private Sensor aSensor;
     private float[] mValues;
     private long time = 0L;
-    private TextView printTextView,setTextView,clearTextView,locationTextView;
+    private TextView printTextView,setTextView,clearTextView,locationTextView,getTextView,demoTextView;
+    private EditText idEditText;
+    private TextView sendTextView;
     private List<LatLng> points =  new ArrayList<>();
     private List<Polygon> polygons = new ArrayList<>();
-    private double radius = 0.01;
+    private double radius = 1;
     private Marker marker;
-
+    private Handler handler;
+    private SensorManager manager;
+    private String bestProvider = LocationManager.GPS_PROVIDER;
+    private boolean isDemo = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        handler = new Handler(getApplicationContext().getMainLooper());
         initMapFragment();
         findView();
         setViewClick();
@@ -70,6 +86,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setTextView = (TextView)findViewById(R.id.setTextView);
         clearTextView = (TextView)findViewById(R.id.clearTextView);
         locationTextView = (TextView)findViewById(R.id.locationTextView);
+        sendTextView = (TextView)findViewById(R.id.sendTextView);
+        idEditText = (EditText)findViewById(R.id.idEditText);
+        getTextView = (TextView)findViewById(R.id.getTextView);
+        demoTextView = (TextView)findViewById(R.id.demoTextView);
     }
 
     private void setViewClick(){
@@ -97,18 +117,115 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 getPermissions();
             }
         });
+        sendTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendData();
+            }
+        });
+        getTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData();
+            }
+        });
+        demoTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                demoTime();
+            }
+        });
+    }
+    private void demoTime(){
+        if(isDemo){
+            isDemo = false;
+        }else{
+            isDemo = true;
+            latitude = 25.021909;
+            longitude = 121.534992;
+            addNewMark(latitude, longitude);
+        }
+    }
+    private void sendData(){
+        if(points.size()<2)Toast.makeText(this,"請標示兩個點",Toast.LENGTH_SHORT).show();
+        String id = idEditText.getText().toString();
+        if(id.length() == 0)Toast.makeText(this,"請輸入Id",Toast.LENGTH_SHORT).show();
+        JSONObject jsonObject = new JSONObject();
+        try{
+            jsonObject.put("id",id);
+            jsonObject.put("firstx",points.get(0).latitude);
+            jsonObject.put("firsty",points.get(0).longitude);
+            jsonObject.put("secondx",points.get(1).latitude);
+            jsonObject.put("secondy",points.get(1).longitude);
+            jsonObject.put("myx",latitude);
+            jsonObject.put("myy",longitude);
+            ConnectService.sendPost(SEND_POINTS_URL, jsonObject.toString(), new ConnectService.Callback() {
+                @Override
+                public void callback(boolean success, String json) {
+                    if(success){
+                        Toast.makeText(MapsActivity.this,"上傳成功",Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(MapsActivity.this,"上傳失敗",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            },handler);
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    private void getData(){
+        ConnectService.sendGet(GET_POINTS_URL, new ConnectService.Callback() {
+            @Override
+            public void callback(boolean success, String json) {
+                if(success){
+                    try {
+                        clear();
+                        parseData(new JSONArray(json));
+                        Toast.makeText(MapsActivity.this,"下載成功",Toast.LENGTH_SHORT).show();
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                }else{
+                    Toast.makeText(MapsActivity.this,"下載失敗",Toast.LENGTH_SHORT).show();
+                }
+            }
+        },handler);
+    }
+    private void parseData(JSONArray array){
+        Random random = new Random();
+        for(int i =0;i<array.length();i++){
+            try{
+                JSONObject jsonObject = array.getJSONObject(i);
+                List<LatLng> points =  new ArrayList<>();
+                points.add(new LatLng(jsonObject.getDouble("firstx"),jsonObject.getDouble("firsty")));
+                points.add(new LatLng(jsonObject.getDouble("secondx"),jsonObject.getDouble("secondy")));
+                points.add(new LatLng(jsonObject.getDouble("myx"),jsonObject.getDouble("myy")));
+
+                int color = random.nextInt() % 0x00FFFFFF;
+                color += 0x66000000;
+                print(color,points);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
     private void setmValues(){
         double x = mValues[0];
         x = x * Math.PI / 180.0 ;
         points.add(new LatLng( Math.cos(x) * radius + latitude,  Math.sin(x) * radius + longitude));
     }
+    private void print(int color,List<LatLng> points){
+        Polygon polygon = mMap.addPolygon(new PolygonOptions().addAll(points)
+                .strokeColor(0x00FFFFFF)
+                .fillColor(color));
+        polygons.add(polygon);
+    }
     private void print(){
-        for(LatLng latLng : points){
-            Log.e(this.getClass().getName(),"lat : "+latLng.latitude+", lon : "+latLng.longitude);
-        }
         Polygon polygon = mMap.addPolygon(new PolygonOptions().addAll(points).add(new LatLng(latitude,longitude))
-                .strokeColor(Color.RED)
+                .strokeColor(0x00FFFFFF)
                 .fillColor(0x660000FF));
         polygons.add(polygon);
     }
@@ -136,7 +253,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-
     private void getSensor() {
         SensorManager manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         //得到方向传感器
@@ -162,6 +278,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         };
         manager.registerListener(mListener, mSensor, SensorManager.SENSOR_DELAY_GAME);
 
+
     }
 
     private LocationManager lms;
@@ -170,6 +287,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         lms = (LocationManager) getSystemService(LOCATION_SERVICE);    //取得系統定位服務
         try {
             Location location = lms.getLastKnownLocation(LocationManager.GPS_PROVIDER);    //使用GPS定位座標
+            lms.requestLocationUpdates(bestProvider, 1000, 1, this);
             getLocation(location);
         } catch (SecurityException e) {
             e.printStackTrace();
@@ -177,10 +295,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void getLocation(Location location) {    //將定位資訊顯示在畫面中
+        if(isDemo)return;
         if (location != null) {
             longitude = location.getLongitude();    //取得經度
             latitude = location.getLatitude();    //取得緯度
-            onMapReady(mMap);
+            addNewMark(latitude,longitude);
         } else {
             Toast.makeText(this, "無法定位座標", Toast.LENGTH_LONG).show();
         }
@@ -224,13 +343,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         if (googleMap == null) return;
         mMap = googleMap;
+        addNewMark(latitude,longitude);
+    }
+
+    private void addNewMark(double latitude,double longitude){
+        if(mMap == null)return;
         if(marker != null)marker.remove();
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(latitude, longitude);
 
-
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         marker = mMap.addMarker(new MarkerOptions().position(sydney).title("My Location"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.e(MapsActivity.class.getName(),"location change");
+        getLocation(location);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
